99acbdc9f1a1478a03f7dc138b95b624f172ad83
Oleksandr Tkachov <131679190+tkachovua@users.noreply.github.com>
2023-06-23 12:24:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "gke_cluster" {
   source         = "github.com/tkachovua/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
 }
