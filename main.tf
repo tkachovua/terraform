e7b40501c04841cda22a8800556db506a983e396
Oleksandr T <oleksandr.tkachov.ua@gmail.com>
2023-06-28 17:30:14 +0300

    gke

--- main.tf
+++ main.tf
@@ -18,7 +18,10 @@ module "flux_bootstrap" {
   source            = "./modules/tf-fluxcd-flux-bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
-  config_path       = module.gke_cluster.kubeconfig
+  config_host       = module.gke_cluster.config_host
+  config_token      = module.gke_cluster.config_token
+  config_ca         = module.gke_cluster.config_ca
+  github_token      = var.GITHUB_TOKEN
 }
 
 module "tls_private_key" {
