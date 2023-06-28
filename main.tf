832fd0eeab05fe764a75ce04528a7a994c0d8d91
Oleksandr T <oleksandr.tkachov.ua@gmail.com>
2023-06-28 15:51:00 +0300

    Update

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
-provider "google" {
-  project = var.GOOGLE_PROJECT
-  region  = var.GOOGLE_REGION
+module "github_repository" {
+  source                   = "./modules/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux-key"
 }
 
 module "gke_cluster" {
@@ -8,6 +12,13 @@ module "gke_cluster" {
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = var.GKE_NUM_NODES
+}
+
+module "flux_bootstrap" {
+  source            = "./modules/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.gke_cluster.kubeconfig
 }
 
 module "tls_private_key" {
--- variables.tf
+++ variables.tf
@@ -13,3 +13,25 @@ variable "GKE_NUM_NODES" {
   default     = 1
   description = "GKE nodes number"
 }
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "The GitHub owner repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repo"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
+}
